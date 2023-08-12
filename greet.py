280be466408a1d93f3857aae04e2fcdca6b658a3
Surya-827 <vsuryakiran827@gmail.com>
2023-08-12 19:06:53 +0530

    first commit

---
+++ greet.py
@@ -0,0 +1,5 @@
+username = input("Enter User Name : ")
+print(f"Entered User Name : {username}")
+print(
+    f"User name in Upper Case : {username.upper()} \n User name in Lower case:  {username.lower()} \n User name in Capitalize Case : {username.capitalize()}"
+    )
